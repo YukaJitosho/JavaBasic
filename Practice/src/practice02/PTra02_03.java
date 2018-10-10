1e4d08870587c58f645d940791d004d56d54af3e
DESKTOP-CPIIR7V\LIKEIT_STUDENT <yuka.jitosho.likeit@gmail.com>
2018-10-10 16:43:40 +0900

    2-2

--- Practice/src/practice02/PTra02_03.java
+++ Practice/src/practice02/PTra02_03.java
@@ -9,10 +9,10 @@ package practice02;
 public class PTra02_03 {
 	public static void main(String[] args) {
 		int num1 = 50;
-		System.out.println(num1);
+		
 
 		// ★ 演算の評価の順番を変更して、num2に140が入るようにしてください(先に足し算が行われるよう変更)
-		int num2 = 20 + num1 * 2;
+		int num2 = (20 + num1) * 2;
 
 		// 140が表示されること
 		System.out.println(num2);	// ※※ この行は修正しないでください

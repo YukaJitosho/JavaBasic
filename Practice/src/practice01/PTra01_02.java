aa425027222e5eef09b627522e7707df1b88d70f
DESKTOP-CPIIR7V\LIKEIT_STUDENT <yuka.jitosho.likeit@gmail.com>
2018-10-10 14:17:38 +0900

    no message

--- Practice/src/practice01/PTra01_02.java
+++ Practice/src/practice01/PTra01_02.java
@@ -10,9 +10,11 @@ public class PTra01_02 {
 	public static void main(String[] args) {
 
 		// ★ 12～14行目をコメントにしてください。
-		ここをコメントにしてください
-		ここをコメントにしてください
-		ここをコメントにしてください
+
+
+		 System.out.println("Hello word");
+
+
 
 		//  ★「Hello, world」と出力（コマンドプロンプトに表示）してください
 
